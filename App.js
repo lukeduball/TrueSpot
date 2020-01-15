edfba403ce9d7b2ff573a4cdeac8e59ff3bece2c
Luke <lukeduball09@gmail.com>
2020-01-15 12:21:48 -0600

    Added a method to deal with lastTouchDistance being zero, created last touch distance as local variable

--- App.js
+++ App.js
@@ -14,6 +14,7 @@ class MapImage extends Component
   constructor(props)
   {
     super(props);
+    this.lastTouchDistance = 0.0;
     var w = 1473;
     var x = (this.props.screenWidth - w) / 2;
     this.state = {xPos: x,
@@ -21,7 +22,6 @@ class MapImage extends Component
                   width: w, 
                   height: 1652, 
                   rotation: 0, 
-                  lastTouchDistance: 0.0, 
                   lastPanX: 0, 
                   lastPanY: 0};
   }
@@ -33,7 +33,7 @@ class MapImage extends Component
       var touchDiffX = evt.nativeEvent.touches[0].locationX - evt.nativeEvent.touches[1].locationX;
       var touchDiffY = evt.nativeEvent.touches[0].locationY - evt.nativeEvent.touches[1].locationY;
       var distance = Math.sqrt(touchDiffX * touchDiffX + touchDiffY * touchDiffY);
-      this.setState({lastTouchDistance: distance})
+      this.lastTouchDistance = distance;
       return true;
     }
     else if(evt.nativeEvent.touches.length == 1)
@@ -52,7 +52,12 @@ class MapImage extends Component
       var touchDiffX = evt.nativeEvent.touches[0].locationX - evt.nativeEvent.touches[1].locationX;
       var touchDiffY = evt.nativeEvent.touches[0].locationY - evt.nativeEvent.touches[1].locationY;
       var distance = Math.sqrt(touchDiffX * touchDiffX + touchDiffY * touchDiffY);
-      var zoom = distance / this.state.lastTouchDistance;
+      this.lastTouchDistance = distance;
+      if(lastTouchDistance === 0)
+      {
+        return;
+      }
+      var zoom = distance / this.lastTouchDistance;
       var newWidth = this.state.width * zoom;
       var newHeight = this.state.height * zoom;
       if(newWidth < this.props.screenWidth)
@@ -67,15 +72,15 @@ class MapImage extends Component
         newWidth = this.props.maxWidth;
         newHeight = ratio * newWidth;
       }
-      this.setState({width: newWidth, height: newHeight, lastTouchDistance: distance});
+      this.setState({width: newWidth, height: newHeight});
     }
     else if(evt.nativeEvent.touches.length == 1)
     {
       var diffX = evt.nativeEvent.touches[0].locationX - this.state.lastPanX;
       var diffY = evt.nativeEvent.touches[0].locationY - this.state.lastPanY;
-      var dampner = 1.0;
-      this.setState({xPos: this.state.xPos + diffX,
-                     yPos: this.state.yPos + diffY,
+      var dampner = 0.1;
+      this.setState({xPos: this.state.xPos + diffX * dampner,
+                     yPos: this.state.yPos + diffY * dampner,
       });
       console.log('Pan X: ' + diffX + ' Y: ' + diffY);
     }
@@ -96,8 +101,8 @@ class MapImage extends Component
           {this.props.children}
         <Image
           style = {{
-          top: this.state.xPos,
-          left: this.state.yPos,
+          top: this.state.yPos,
+          left: this.state.xPos,
           width: this.state.width,
           height: this.state.height,
           transform: [{rotate: this.state.rotation+'deg'}],
