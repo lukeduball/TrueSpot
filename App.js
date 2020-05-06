dfec243ea2a0908bec073ff978164980c0022858
Luke <lukeduball09@gmail.com>
2020-05-06 15:18:35 -0500

    Removed unused style sheet

--- App.js
+++ App.js
@@ -55,18 +55,3 @@ export default class App extends Component
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  box: {
-    backgroundColor: '#cc0000',
-    width: 200,
-    height: 200,
-    borderRadius: 5,
-  }
-});
